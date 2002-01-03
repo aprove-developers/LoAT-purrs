2fe1435284532426f62073565828f633e9a03db3
roberto <roberto>
2002-01-03 17:57:50 +0000

    Cleaned.

--- tests/eqs_driver.cc
+++ tests/eqs_driver.cc
@@ -9,31 +9,10 @@
 
 using namespace GiNaC;
 
-void
-solve_equation_4(const GNumber& a1, const GNumber& a2,
-		 const GNumber& a3, const GNumber& a4,
-		 GExpr& x1, GExpr& x2, GExpr& x3, GExpr& x4);
-
-void foo() {
-  GExpr x1, x2, x3, x4;
-  solve_equation_4(0, -2, 0, 1, x1, x2, x3, x4);
-  cout << "x1 = " << x1 << endl;
-  cout << "****** " << x1.evalf() << endl;
-  cout << "x2 = " << x2 << endl;
-  cout << "****** " << x2.evalf() << endl;
-  cout << "x3 = " << x3 << endl;
-  cout << "****** " << x3.evalf() << endl;
-  cout << "x4 = " << x4 << endl;
-  cout << "****** " << x4.evalf() << endl;
-}
-
 int
 main() try {
   GSymbol x("x");
   GList l(x);
-
-  foo();
-
   GExpr p;
   while (cin) {
     string s;
@@ -52,7 +31,7 @@ main() try {
       for (size_t i = 0; i < n; ++i) {
 	cout << "x_" << i << " = " << roots[i] << endl;
 	if (!is_a<numeric>(roots[i]))
-	  cout << "  x_" << i << " = " << roots[i].evalf() << endl;
+	  cout << "****  x_" << i+1 << " ~= " << roots[i].evalf() << endl;
       }
     }
   }
