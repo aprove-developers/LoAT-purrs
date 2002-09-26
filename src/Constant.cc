5356a5f989d650c2311f578626ee5da44dcb1c3e
roberto <roberto>
2002-09-26 10:48:10 +0000

    New class for constant expressions.

---
+++ src/Constant.cc
@@ -0,0 +1,31 @@
+/* Constant class implementation (non-inline functions).
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/purrs/ . */
+
+#include <config.h>
+
+#include "Constant.defs.hh"
+
+namespace PURRS = Parma_Recurrence_Relation_Solver;
+
+const PURRS::Constant PURRS::Pi = GiNac::Pi;
+
---
+++ src/Constant.inlines.hh
@@ -0,0 +1,56 @@
+/* *******************: a recurrence: inline functions.
+   Copyright (C) 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma University's Recurrence Relation
+Solver (PURRS).
+
+The PURRS is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PURRS is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the PURRS site:
+http://www.cs.unipr.it/purrs/ . */
+
+#ifndef PURRS_Constant_inlines_hh
+#define PURRS_Constant_inlines_hh
+
+namespace Parma_Recurrence_Relation_Solver {
+
+inline
+Constant::Constant() {
+}
+
+inline
+Constant::Constant(const Constant& k)
+  : c(k.c) {
+};
+
+inline Constant&
+Constant::operator=(const Constant& k) {
+  c = k.c;
+  return *this;
+};
+
+inline
+Constant::Constant(const GiNaC::constant& gc)
+  : c(gc) {
+}
+
+inline
+Constant::~Constant() {
+}
+
+} // namespace Parma_Recurrence_Relation_Solver
+
+#endif // !defined(PURRS_Constant_inlines_hh)
