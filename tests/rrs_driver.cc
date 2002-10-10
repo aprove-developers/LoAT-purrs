0f92f769b005f83399520ae8ffde49d5821601b9
roberto <roberto>
2002-10-10 17:50:19 +0000

    Accepts options so as to serve both as a debugging tool and as a test program for `make check'.

--- tests/rrs_driver.cc
+++ tests/rrs_driver.cc
@@ -26,8 +26,10 @@ http://www.cs.unipr.it/purrs/ . */
 #include <string>
 #include <stdexcept>
 #include <cassert>
+#include <getopt.h>
 
 #include "purrs_install.hh"
+#include "ehandlers.hh"
 
 #ifdef USE_READLINE
 #include "readlinebuf.hh"
@@ -37,20 +39,84 @@ http://www.cs.unipr.it/purrs/ . */
 using namespace std;
 using namespace Parma_Recurrence_Relation_Solver;
 
-#define NOISY 1
+static struct option long_options[] = {
+  {"help",           no_argument,       0, 'h'},
+  {"interactive",    no_argument,       0, 'i'},
+  {0, 0, 0, 0}
+};
 
-#ifdef USE_READLINE
-#define INPUT_STREAM rdl
-#else
-#define INPUT_STREAM cin
-#endif
+static const char* usage_string
+= "Usage: %s [OPTION]...\n\n"
+"  -h, --help              prints this help text\n"
+"  -i, --interactive       sets interactive mode on\n";
+
+#define OPTION_LETTERS "hi"
+
+// Interactive mode is on when true.
+static bool interactive = false;
+
+static void
+my_exit(int status) {
+  //(void) purrs_finalize();
+  exit(status);
+}
+
+static void
+process_options(int argc, char* argv[]) {
+  int option_index;
+  int c;
+
+  while (1) {
+    option_index = 0;
+    c = getopt_long(argc, argv, OPTION_LETTERS, long_options, &option_index);
+    if (c == EOF)
+      break;
+
+    switch (c) {
+    case 0:
+      break;
+
+    case '?':
+    case 'h':
+      fprintf(stderr, usage_string, argv[0]);
+      my_exit(0);
+      break;
+
+    case 'i':
+      interactive = 1;
+      break;
+
+    default:
+      abort();
+    }
+  }
+
+  if (optind < argc) {
+    fprintf(stderr, usage_string, argv[0]);
+    my_exit(1);
+  }
+}
 
 int
-main() try {
+main(int argc, char *argv[]) try {
+  set_handlers();
+
+  process_options(argc, argv);
+
+  readlinebuf* prdlb = 0;
+  istream* pinput_stream;
+
 #ifdef USE_READLINE
-  auto_ptr<readlinebuf> rdlb(new readlinebuf());
-  istream rdl(rdlb.get());
+  if (interactive) {
+    prdlb = new readlinebuf();
+    pinput_stream = new istream(prdlb);
+  }
+  else
+    pinput_stream = &cin;
+#else
+  pinput_stream = &cin;
 #endif
+  istream& input_stream = *pinput_stream;
 
   Symbol n("n");
   Symbol a("a");
@@ -59,11 +125,11 @@ main() try {
   Symbol d("d");
   Expr_List symbols(n, a, b, c, d);
   Expr rhs;
-  while (INPUT_STREAM) {
+  while (input_stream) {
     string s;
-    getline(INPUT_STREAM, s);
+    getline(input_stream, s);
 
-    if (!INPUT_STREAM)
+    if (!input_stream)
       return 0;
 
     // Skip comments.
@@ -72,18 +138,15 @@ main() try {
 
     rhs = Expr(s, symbols);
 
-#if NOISY
-    cout << "Trying to solve x(n) = " << rhs << endl;
-#endif
+    if (interactive)
+      cout << "Trying to solve x(n) = " << rhs << endl;
 
     Expr solution;
     if (solve_try_hard(rhs, n, solution) != OK) {
-#if NOISY
-      cout << "Sorry, this is too difficult." << endl;
-#endif
+      if (interactive)
+	cout << "Sorry, this is too difficult." << endl;
     }
-#if NOISY
-    else {
+    else if (interactive) {
       cout << "*** SOLUTION ***"
 	   << endl
 	   << solution
@@ -91,15 +154,14 @@ main() try {
 	   << "****************"
 	   << endl << endl;
     }
-#endif
   }
   return 0;
 } 
 catch (exception &p) {
-#if NOISY
-  cerr << "Exception caught: " << p.what() << endl;
-#endif
+  cerr << "std::exception caught: " << p.what() << endl;
+  return 1;
 }
 catch (const char* s) {
   cerr << s << endl;
+  return 1;
 }
