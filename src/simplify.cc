72ecb3f2e4db39588d3794cd974e1e30124d1416
zolo <zolo>
2003-01-08 14:35:07 +0000

    Added `simplify_numer_denom()' at the end of the function `simplify_all()'.

--- src/simplify.cc
+++ src/simplify.cc
@@ -1158,6 +1158,7 @@ PURRS::simplify_all(const Expr& e) {
   e_rewritten = simplify_numer_denom(e_rewritten);
   e_rewritten = simplify_factorials_and_exponentials(e_rewritten);
   e_rewritten = simplify_on_output_ex(e_rewritten.expand(), false);
+  e_rewritten = simplify_numer_denom(e_rewritten);
   e_rewritten = e_rewritten.expand();
   return e_rewritten;
 }
