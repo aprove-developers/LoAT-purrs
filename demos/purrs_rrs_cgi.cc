d90960e96ec33a500a4337b48924aaaf1a324ce4
zolo <zolo>
2003-01-21 09:33:00 +0000

    Fixed a bug: now is used the special symbol `n' in unique copy.

--- demos/purrs_rrs_cgi.cc
+++ demos/purrs_rrs_cgi.cc
@@ -237,12 +237,11 @@ main() try {
   if(expr == (*cgi).end() || expr->isEmpty())
     error("you did not type anything!!!");
 
-  Symbol n("n");
   Symbol a("a");
   Symbol b("b");
   Symbol c("c");
   Symbol d("d");
-  Expr_List symbols(n, a, b, c, d);
+  Expr_List symbols(Recurrence::n, a, b, c, d);
   Expr rhs = Expr(**expr, symbols);
   Recurrence recurrence(rhs);
 
