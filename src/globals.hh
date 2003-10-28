0562834e9ee722602bbebc9bd0aa13610c1c417a
zolo <zolo>
2003-10-28 17:15:59 +0000

    Added the file containing the declarations of global objects. At the moment it contains `index_type': an unsigned integral type for representing different kinds of indexes of the recurrence.

---
+++ src/globals.hh
@@ -0,0 +1,44 @@
+/* Declarations of global objects.
+   Copyright (C) 2001-2003 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PURRS).
+
+The PURRS is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PURRS is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/purrs/ . */
+
+#ifndef PURRS_globals_hh
+#define PURRS_globals_hh 1
+
+namespace Parma_Recurrence_Relation_Solver {
+
+//! \brief
+//! An unsigned integral type for representing different kinds of
+//! indexes of the recurrence.
+/*!
+  Are <CODE>index_type</CODE> the following elements:
+  - the order of a recurrence;
+  - the rank of a functional equation;
+  - the smallest positive integer starting from which the recurrence,
+    or the functional equation, is well-defined (the initial
+    conditions will start from it).
+*/
+typedef unsigned int index_type;
+
+} // namespace Parma_Recurrence_Relation_Solver
+
+#endif
