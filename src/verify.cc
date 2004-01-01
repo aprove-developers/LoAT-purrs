97389ffc5b6b93c886aa1c5a96de6dd4670d5138
zolo <zolo>
2004-01-01 18:52:44 +0000

    Minor changes.

--- src/verify.cc
+++ src/verify.cc
@@ -123,37 +123,6 @@ validate_initial_conditions(index_type order) const {
 PURRS::Recurrence::Verify_Status
 PURRS::Recurrence::
 traditional_step_3(index_type order_rec, const Expr& e) const {
-#if 0
-  // Step 3: by substitution, verifies that `e'
-  // satisfies the homogeneous part of the recurrence.
-  // Computes `substituted_homogeneous_rhs' by substituting, in the
-  // hoomogeneous part of the recurrence, `n' by `n - d' (where `d' is
-  // the decrement of the i-th term `a_i(n)*x(n - d)').
-  Expr substituted_homogeneous_rhs = recurrence_rhs - inhomogeneous_term;
-  // Substitutes in the homogeneous part of the recurrence the terms
-  // of the form `x(n-i)'.
-  //for (index_type d = 1; d <= order_rec; d = d + gcd) {
-  for (index_type d = 1; d <= order_rec; ++d) {
-    Expr shifted_solution
-      = simplify_all(e.substitute(n, n - d));
-    shifted_solution = simplify_sum(shifted_solution, REWRITE_UPPER_LIMIT);
-    substituted_homogeneous_rhs
-      = substituted_homogeneous_rhs.substitute(x(n - d), shifted_solution);
-  }
-  Expr diff = e - substituted_homogeneous_rhs;
-  // Differently from the step 1 (validation of symbolic initial condition)
-  // the expression `diff' now contains `n' and is more difficult
-  // to simplify it. For this motive we performed simplification also
-  // before to expand blackboard's definitions.
-  diff = blackboard.rewrite(diff);
-  diff = simplify_all(diff);
-  if (!diff.is_zero()) {
-    diff = simplify_all(diff);
-    if (!diff.is_zero())
-      return INCONCLUSIVE_VERIFICATION;
-  }
-  return PROVABLY_CORRECT;
-#else
   // By substitution, verifies that `e' satisfies the homogeneous part
   // of the recurrence.
   // Computes `substituted_homogeneous_rhs' by substituting, in the
@@ -183,59 +152,61 @@ traditional_step_3(index_type order_rec, const Expr& e) const {
       return INCONCLUSIVE_VERIFICATION;
   }
   return PROVABLY_CORRECT;
-#endif
 }
 
+// FIXME: to finish comment!!!
+/*!
+  Find the maximum degree of a polynomial that may occur in the
+  solution in the following way:
+  we assume that \f$ \lambda_1 \f$, ..., \f$ \lambda_m \f$ are the
+  roots of the characteristic equation, of multiplicity
+  \f$ r_1 \f$, ..., \f$ r_m \f$ respectively, where \f$ \lambda_i \f$
+  are complex numbers all distict and  the multiplicity are positive
+  integer (\f$ r_1 +...+ r_m = k \f$ where \f$ k \f$ is the order
+  of the recurrence).
+  Moreover, we assume that the non-homogeneous part of the recurrence
+  has the following form
+  \f[
+    p_1(n) \alpha_1^n + ... + p_j(n) \alpha_j^n
+  \f]
+  where \f$ \alpha_i \f$ are complex numbers all distinct and the
+  \f$ p_i \f$ are polynomials (different from \f$ 0 \f$).
+  We set
+  \f[
+    q_1 =
+    \begin{cases}
+      r_1 - 1,
+         \quad \text{if } \lambda_1 \notin \{ \alpha_1, ..., \alpha_j \}\\
+      r_1 - 1, + \deg(p_i)
+         \quad \text{if } \lambda_1 = \alpha_i.\\
+    \end{cases}
+  \f]
+  Analogously for \f$ q_2 \f$, ..., \f$ q_m \f$.
+  The maximum degree of a polynomial that may occur in the
+  solution is the maximum of the set
+  \f[
+    \{ q_1, ..., q_m, \deg(p_1), ..., \deg(p_j) \}.
+  \f]
+  Since \f$ r_i \le k \f$ is possible to consider the MAGGIORAZIONE
+  \f[
+    k - 1 + \max \{ \deg(p_1), ..., \deg(p_j) \}.
+  \f]
+  This method can seem inefficient because builds memory's locations
+  that will not be used, but is not necessary the comparison between
+  the roots \f$ \lambda_i \f$ and the bases of the exponentials
+  \f$ \alpha_j \f$ (we also observe that in the memory's locations
+  unused there is \f$ 0 \f$ and so we can avoid useless checks.
+  In the following code we use the MAGGIORAZIONE
+  \f[
+    max_polynomial_degree
+      =\max\{ r_1, ..., r_m \} - 1 + \max \{ \deg(p_1), ..., \deg(p_j) \}.
+  \f]
 bool
 PURRS::Recurrence::
 verify_new_method_const_coeff(index_type order_rec, const Expr& e,
 			      const std::vector<Polynomial_Root> roots,
 			      bool inhomogeneous_part) const {
-  // Find the maximum degree of a polynomial that may occur in the
-  // solution in the following way:
-  // we assume that \f$ \lambda_1 \f$, ..., \f$ \lambda_m \f$ are the
-  // roots of the characteristic equation, of multiplicity
-  // \f$ r_1 \f$, ..., \f$ r_m \f$ respectively, where \f$ \lambda_i \f$
-  // are complex numbers all distict and  the multiplicity are positive
-  // integer (\f$ r_1 +...+ r_m = k \f$ where \f$ k \f$ is the order
-  // of the recurrence).
-  // Moreover, we assume that the non-homogeneous part of the recurrence
-  // has the following form
-  // \f[
-  //   p_1(n) \alpha_1^n + ... + p_j(n) \alpha_j^n
-  // \f]
-  // where \f$ \alpha_i \f$ are complex numbers all distinct and the
-  // \f$ p_i \f$ are polynomials (different from \f$ 0 \f$).
-  // We set
-  // \f[
-  //   q_1 =
-  //   \begin{cases}
-  //     r_1 - 1,
-  //        \quad \text{if } \lambda_1 \notin \{ \alpha_1, ..., \alpha_j \}\\
-  //     r_1 - 1, + \deg(p_i)
-  //        \quad \text{if } \lambda_1 = \alpha_i.\\
-  //   \end{cases}
-  // \f]
-  // Analogously for \f$ q_2 \f$, ..., \f$ q_m \f$.
-  // The maximum degree of a polynomial that may occur in the
-  // solution is the maximum of the set
-  // \f[
-  //   \{ q_1, ..., q_m, \deg(p_1), ..., \deg(p_j) \}.
-  // \f]
-  // Since \f$ r_i \le k \f$ is possible to consider the MAGGIORAZIONE
-  // \f[
-  //   k - 1 + \max \{ \deg(p_1), ..., \deg(p_j) \}.
-  // \f]
-  // This method can seem inefficient because builds memory's locations
-  // that will not be used, but is not necessary the comparison between
-  // the roots \f$ \lambda_i \f$ and the bases of the exponentials
-  // \f$ \alpha_j \f$ (we also observe that in the memory's locations
-  // unused there is \f$ 0 \f$ and so we can avoid useless checks.
-  // In the following code we use the MAGGIORAZIONE
-  // \f[
-  //   max_polynomial_degree
-  //     =\max\{ r_1, ..., r_m \} - 1 + \max \{ \deg(p_1), ..., \deg(p_j) \}.
-  // \f]
+  // The maximum degree of a polynomial that may occur in the solution.
   unsigned int max_polynomial_degree = 0;
   unsigned int num_of_exponentials = 0;
   if (inhomogeneous_part) {
